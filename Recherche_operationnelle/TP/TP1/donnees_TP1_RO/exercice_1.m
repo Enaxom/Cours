79e586f9d6f0d6df9c1daebcb1191b0ce7d79c9c
Enaxom <enaxom@gmail.com>
2019-09-27 14:43:55 +0200

    ro

--- Recherche_operationnelle/TP/TP1/donnees_TP1_RO/exercice_1.m
+++ Recherche_operationnelle/TP/TP1/donnees_TP1_RO/exercice_1.m
@@ -1,13 +1,13 @@
 % Objectif du programme lineaire P1 :
-C = ...
+C = [-10000 -9000];
 
 % Contraintes du programme lineaire P1 :
-A = ...
-B = ...
-Binf = ...
+A = [0.06 0.05;10 20;1 0];
+B = [60;15000;800];
+Binf = [0;0];
 
 % Resolution du programme lineaire P1 :
-[X,zmin] = linprog(...);
+[X,zmin] = linprog(C,A,B,[],[],Binf,[]);
 
 % Affichage du resultat :
 fprintf('\nLa solution vaut (%.3f,%.3f)\n',X);
