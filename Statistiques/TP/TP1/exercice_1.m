b1e126ed76cc8a064a6b057d6915a912daf414d8
Enaxom <enaxom@gmail.com>
2019-10-11 14:27:56 +0200

    stat

--- Statistiques/TP/TP1/exercice_1.m
+++ Statistiques/TP/TP1/exercice_1.m
@@ -27,25 +27,42 @@ xlabel('Niveau de gris','FontSize',15);
 ylabel('Frequence absolue','FontSize',15);
 set(gca,'FontSize',20);
 
-% Definition de la sŽrie statistique 2D :
-% I_gauche = ...
-% ...
-% I_droite = ...
-% ...
+% Definition de la sï¿½rie statistique 2D :
+% Les matrices I et J contiennent les numeros de lignes et de colonnes des pixels:
+% gris = double(rgb2gray(I));
+[Y,X] = meshgrid(1:nb_colonnes,1:nb_lignes);
+N = nb_lignes*nb_colonnes;
+I_gauche = zeros(1,N-1);
+index = 0;
+for i = 1:nb_lignes
+    for j = 1:nb_colonnes-1
+        index = index + 1;
+        I_gauche(index) = I(i,j);
+    end
+end
+
+I_droite = zeros(1,N-1);
+index = 0;
+for i = 1:nb_lignes
+    for j = 2:nb_colonnes
+        index = index + 1;
+        I_droite(index) = I(i,j);
+    end
+end
 
 % Affichage de la serie statistique 2D sous la forme d'un nuage de points :
-% figure('Name','Mise en evidence de la correlation entre pixels voisins','Position',[0.66*L,0,0.33*L,0.45*H]);
-% plot(I_gauche,I_droite,'b+');
-% axis([I_min I_max I_min I_max]);
-% xlabel('Niveau de gris du pixel de gauche','FontSize',15);
-% ylabel('Niveau de gris du pixel de droite','FontSize',15);
-% hold on;
-% set(gca,'FontSize',20);
-% axis equal;
+figure('Name','Mise en evidence de la correlation entre pixels voisins','Position',[0.66*L,0,0.33*L,0.45*H]);
+plot(I_gauche,I_droite,'b+');
+axis([I_min I_max I_min I_max]);
+xlabel('Niveau de gris du pixel de gauche','FontSize',15);
+ylabel('Niveau de gris du pixel de droite','FontSize',15);
+hold on;
+set(gca,'FontSize',20);
+axis equal;
 
 % Calcul du coefficient de correlation lineaire :
-% moyenne_abscisses = mean(I_gauche);
-% moyenne_ordonnees = mean(I_droite);
+moyenne_abscisses = mean(I_gauche);
+moyenne_ordonnees = mean(I_droite);
 % variance_abscisses = ...
 % variance_ordonnees = ...
 % covariance = ...
